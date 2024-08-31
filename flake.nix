6045554f230b80a7cfc66bbef6fefa9c14c907b6
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-31 22:51:20 +0200

    base: add initial structure

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    nixvim,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: let
+        nixvim' = nixvim.legacyPackages.${system};
+        nvim = nixvim'.makeNixvimWithModule {
+          inherit pkgs;
+          module = ./config;
+        };
+      in {
+        packages.default = nvim;
+
+        devShells = {
+          default = with pkgs; mkShell;
+        };
+      };
+    };
+}

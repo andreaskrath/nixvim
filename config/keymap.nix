a22baebed2589ea12301f78ade92b6126a182f41
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-01 23:17:05 +0100

    keymap: add helix-like keymaps for jumping to start and end of line

--- config/keymap.nix
+++ config/keymap.nix
@@ -4,6 +4,61 @@ in {
   globals.mapleader = " ";
 
   keymaps = [
+    {
+      mode = normal;
+      key = "gs";
+      action = "^";
+      options = {
+        desc = "Go to first no whitespace character";
+        noremap = true;
+        silent = true;
+      };
+    }
+
+    {
+      mode = normal;
+      key = "gh";
+      action = "0";
+      options = {
+        desc = "Go to start of line";
+        noremap = true;
+        silent = true;
+      };
+    }
+
+    {
+      mode = normal;
+      key = "gl";
+      action = "$";
+      options = {
+        desc = "Go to end of line";
+        noremap = true;
+        silent = true;
+      };
+    }
+
+    {
+      mode = normal;
+      key = "0";
+      action = "<Nop>";
+      options = {
+        desc = "Disable 0 for jumping to start of line";
+        noremap = true;
+        silent = true;
+      };
+    }
+
+    {
+      mode = normal;
+      key = "$";
+      action = "<Nop>";
+      options = {
+        desc = "Disable $ for jumping to end of line";
+        noremap = true;
+        silent = true;
+      };
+    }
+
     # nvim-tree
     {
       mode = normal;

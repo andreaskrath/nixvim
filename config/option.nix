938df750f3a5d6bf317d7a273b314fecd0443c50
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-01 23:18:55 +0100

    opt: add some more options

--- config/option.nix
+++ config/option.nix
@@ -4,11 +4,28 @@
     number = true;
     relativenumber = true;
 
+    # tabs and spaces
     tabstop = 2;
     softtabstop = 2;
     showtabline = 2;
     smartindent = true;
     shiftwidth = 2;
     expandtab = true;
+
+    # disable mouse
+    mouse = "";
+
+    updatetime = 0;
+
+    termguicolors = true;
+
+    cursorline = true;
+
+    timeoutlen = 10;
+
+    encoding = "utf-8";
+    fileencoding = "utf-8";
+
+    showmode = false;
   };
 }

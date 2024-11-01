b265594924bf986038a9dabe55bc68bfdfbd2ad4
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-02 00:01:00 +0100

    gruvbox: make background soft-dark and disable bold/italic for most things

--- config/default.nix
+++ config/default.nix
@@ -12,5 +12,25 @@
     ./plugin/treesitter.nix
   ];
 
-  colorschemes.gruvbox.enable = true;
+  colorschemes.gruvbox = {
+    enable = true;
+
+    settings = {
+      terminal_colors = true;
+      undercurl = false;
+      underline = false;
+      bold = false;
+      italic = {
+        strings = false;
+        emphasis = false;
+        comments = true;
+        operators = false;
+        folds = true;
+      };
+
+      palette_overrides = {
+        dark0 = "#32302f";
+      };
+    };
+  };
 }

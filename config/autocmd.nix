585cf243ec168dc5a343d46c4906b6f4e2e8d72f
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-02 20:49:54 +0100

    autocmd: disable semantic syntax highlighting from lsp

--- config/autocmd.nix
+++ config/autocmd.nix
@@ -5,5 +5,18 @@
       event = ["VimEnter"];
       command = "Telescope find_files";
     }
+
+    # disable semantic highlighting
+    {
+      event = ["LspAttach"];
+      callback = {
+        __raw = ''
+          function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            client.server_capabilities.semanticTokensProvider = nil
+          end
+        '';
+      };
+    }
   ];
 }

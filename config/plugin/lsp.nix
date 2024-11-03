d3db5c2270e00f548f6b03fcbe2255f190ac2378
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-03 14:43:30 +0100

    plugin: set nixd and gopls package to null
    prevents auto installing, which I handle through direnv

--- config/plugin/lsp.nix
+++ config/plugin/lsp.nix
@@ -5,10 +5,12 @@
     servers = {
       nixd = {
         enable = true;
+        package = null;
       };
 
       gopls = {
         enable = true;
+        package = null;
       };
     };
   };

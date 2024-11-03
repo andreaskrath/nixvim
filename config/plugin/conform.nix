243caf1e7596d3f2d0de4659a02419b6773235e7
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-03 14:43:55 +0100

    plugin: add rust-analyzer and rustfmt

--- config/plugin/conform.nix
+++ config/plugin/conform.nix
@@ -12,6 +12,7 @@
 
       formatters_by_ft = {
         nix = ["alejandra"];
+        rust = ["rustfmt"];
       };
     };
   };
--- config/plugin/lsp.nix
+++ config/plugin/lsp.nix
@@ -12,6 +12,44 @@
         enable = true;
         package = null;
       };
+
+      rust_analyzer = {
+        enable = true;
+        package = null;
+        installCargo = false;
+        installRustc = false;
+
+        settings = {
+          procMacro.enable = true;
+
+          cargo = {
+            autoreload = true;
+            buildScripts.enable = true;
+          };
+
+          completion = {
+            autoimport.enable = true;
+            autoself.enable = true;
+          };
+
+          check = {
+            command = "clippy";
+            extraArgs = ["--tests" "--" "-W" "clippy::all"];
+          };
+
+          lens = {
+            enable = true;
+            debug.enable = true;
+            implementations.enable = true;
+            references = {
+              adt.enable = true;
+              enumVariant.enable = true;
+              method.enable = true;
+              trait.enable = true;
+            };
+          };
+        };
+      };
     };
   };
 }

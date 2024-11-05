2d6f56a9416ea22fbe2855afea8fe8d1153e38d5
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-05 18:24:38 +0100

    plugin: move lspconfig for rust to rustaceanvim

--- config/plugin/rust.nix
+++ config/plugin/rust.nix
@@ -2,5 +2,42 @@
   plugins.rustaceanvim = {
     enable = true;
     rustAnalyzerPackage = null;
+
+    settings = {
+      tools.test_executor = "backgrounf";
+
+      default_settings = {
+        rust-analyxer = {
+          procMacro.enable = true;
+
+          cargo = {
+            autoreload = true;
+            buildScripts.enable = true;
+          };
+
+          completion = {
+            autoimport.enable = true;
+            autoself.enable = true;
+          };
+
+          check = {
+            command = "clippy";
+            extraArgs = ["--tests" "--" "-W" "clippy::all"];
+          };
+
+          lens = {
+            enable = true;
+            debug.enable = true;
+            implementations.enable = true;
+            references = {
+              adt.enable = true;
+              enumVariant.enable = true;
+              method.enable = true;
+              trait.enable = true;
+            };
+          };
+        };
+      };
+    };
   };
 }

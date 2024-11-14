e97aa04459fa81e1bce9e52d953ffc99ab81e84c
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-14 23:20:25 +0100

    plugin: make telescope borderless
    possible change color slightly some places, but good start

--- config/plugin/telescope.nix
+++ config/plugin/telescope.nix
@@ -1,4 +1,13 @@
-{
+let
+  colors = {
+    bg = "#282828";
+    bg_dark = "#1d2021";
+    bg_light = "#32302f";
+    fg = "#ebdbb2";
+    accent = "#83a598";
+    gray = "#928374";
+  };
+in {
   plugins.telescope = {
     enable = true;
 
@@ -15,6 +24,11 @@
             prompt_position = "top";
           };
         };
+        borderchars = {
+          prompt = [" " " " " " " " " " " " " " " "];
+          results = [" " " " " " " " " " " " " " " "];
+          preview = [" " " " " " " " " " " " " " " "];
+        };
         sorting_strategy = "ascending";
         initial_mode = "normal";
       };
@@ -27,5 +41,31 @@
         }
       '';
     };
+
+    luaConfig.post = ''
+      -- Prompt
+      vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "${colors.accent}", bg = "${colors.bg_dark}" })
+      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "${colors.bg_dark}" })
+      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "${colors.bg_dark}", bg = "${colors.bg_dark}" })
+      vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = "${colors.fg}", bg = "${colors.bg_dark}" })
+
+      -- Results
+      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "${colors.bg}" })
+      vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "${colors.bg}", bg = "${colors.bg}" })
+      vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = "${colors.fg}", bg = "${colors.bg}" })
+      vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "${colors.bg}" })
+      vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "${colors.bg_light}" })
+      vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = "${colors.accent}", bg = "${colors.bg_light}" })
+
+      -- Preview
+      vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "${colors.bg}" })
+      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "${colors.bg}", bg = "${colors.bg}" })
+      vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = "${colors.fg}", bg = "${colors.bg}" })
+
+      -- Other
+      vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "${colors.accent}", bold = true })
+      vim.api.nvim_set_hl(0, "TelescopeMultiSelection", { bg = "${colors.bg_light}" })
+      vim.api.nvim_set_hl(0, "TelescopeCountBorder", { fg = "${colors.bg}", bg = "${colors.bg}" })
+    '';
   };
 }

0b8730808558489ee8c94879aa0774a7e8b15a52
Andreas Krath <andreas.krath+github@gmail.com>
2024-12-07 01:33:56 +0100

    neogit: expand on base configuration

--- config/plugin/neogit.nix
+++ config/plugin/neogit.nix
@@ -2,5 +2,37 @@
   plugins.neogit = {
     enable = true;
     gitPackage = null;
+
+    settings = {
+      disable_hint = true;
+      fetch_after_checkout = true;
+
+      status = {
+        show_head_commit_hash = false;
+      };
+
+      integrations = {
+        telescope = true;
+      };
+
+      sections = {
+        rebase = {
+          folded = false;
+          hidden = false;
+        };
+        recent = {
+          folded = false;
+          hidden = false;
+        };
+        unpulled_upstream = {
+          folded = false;
+          hidden = false;
+        };
+        unpulled_pushRemote = {
+          folded = false;
+          hidden = false;
+        };
+      };
+    };
   };
 }

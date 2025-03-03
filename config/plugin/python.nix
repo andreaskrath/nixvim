7a83fdc760f4196e12c154e7dad1f424362fab3c
Andreas Krath <andreas.krath+github@gmail.com>
2025-03-03 14:12:46 +0100

    plugin: add main debug python configuration

--- config/plugin/python.nix
+++ config/plugin/python.nix
@@ -2,5 +2,13 @@
   plugins.dap-python = {
     enable = true;
     adapterPythonPath = "python3";
+    customConfigurations = [
+      {
+        name = "main";
+        request = "launch";
+        type = "debugpy";
+        program = "./src/main.py";
+      }
+    ];
   };
 }

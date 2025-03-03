9ed4a3d8561f4429375effce9e0616dfcabe68a7
Andreas Krath <andreas.krath+github@gmail.com>
2025-03-03 14:18:05 +0100

    plugin: try more python stuff

--- config/plugin/dap.nix
+++ config/plugin/dap.nix
@@ -1,5 +1,28 @@
 {
   plugins.dap = {
     enable = true;
+
+    configurations = {
+      python = [
+        {
+          type = "python";
+          request = "launch";
+          name = "Project Root (python)";
+          program = "\${file}";
+          cwd = "\${workspacFolder}";
+          justMyCode = true;
+          console = "integratedTerminal";
+        }
+        {
+          type = "debugpy";
+          request = "launch";
+          name = "Project Root (python)";
+          program = "\${file}";
+          cwd = "\${workspacFolder}";
+          justMyCode = true;
+          console = "integratedTerminal";
+        }
+      ];
+    };
   };
 }

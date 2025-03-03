125a1af0474c463653369ec17f8b0edb1e2fa977
Andreas Krath <andreas.krath+github@gmail.com>
2025-03-03 18:11:05 +0100

    plugin: maybe fix everything python (lol no)
    trying a new config 10hi f9s

--- config/plugin/dap.nix
+++ config/plugin/dap.nix
@@ -2,58 +2,18 @@
   plugins.dap = {
     enable = true;
 
-    luaConfig.post = ''
-      local dap = require('dap')
-
-      -- Function to read environment variables from .env file
-      local function load_env_file(env_path)
-        local env_vars = {}
-        local file = io.open(env_path, "r")
-
-        if file then
-          for line in file:lines() do
-            -- Skip comments and empty lines
-            if not line:match("^%s*#") and line:match("%S") then
-              local name, value = line:match("^%s*(%S+)%s*=%s*(.+)%s*$")
-              if name and value then
-                -- Remove quotes if they exist
-                value = value:gsub("^[\"'](.+)[\"']$", "%1")
-                env_vars[name] = value
-              end
-            end
-          end
-          file:close()
-        end
-
-        return env_vars
-      end
-
-      table.insert(dap.configurations.python or {}, {
-        type = 'python',
-        request = 'launch',
-        name = 'Python with .env',
-        program = function()
-          return vim.fn.expand('%:p')
-        end,
-        cwd = function()
-          return vim.fn.getcwd()
-        end,
-        env = function()
-          local project_dir = vim.fn.getcwd()
-          local env_path = project_dir .. '/.env'
-          local env_vars = load_env_file(env_path)
-
-          -- Debug output to verify env vars (can be removed later)
-          print("Loading .env from: " .. env_path)
-          for k, v in pairs(env_vars) do
-            print("ENV: " .. k .. "=" .. v)
-          end
-
-          return env_vars
-        end,
-        justMyCode = false,
-        console = "integratedTerminal",
-      })
-    '';
+    configurations = {
+      python = [
+        {
+          type = "debugpy";
+          request = "launch";
+          name = "Debug";
+          program = "\${file}";
+          cwd = "\${workspaceFolder}";
+          justMyCode = true;
+          console = "integratedTerminal";
+        }
+      ];
+    };
   };
 }
